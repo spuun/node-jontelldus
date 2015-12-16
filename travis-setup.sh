4d3ab7c516b34857488f6eaeb6247d4fbd747b24
Jon Börjesson <jon@spuun.se>
2015-12-16 11:38:58 +0000

    Travis..

--- travis-setup.sh
+++ travis-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-sudo apt-get update -qq
-sudo apt-get install -y libftdi1 libftdi-dev libconfuse0 libconfuse-dev
+#sudo apt-get update -qq
+#sudo apt-get install -y libftdi1 libftdi-dev libconfuse0 libconfuse-dev
 wget http://download.telldus.se/TellStick/Software/telldus-core/telldus-core-2.1.2.tar.gz -O /tmp/telldus-core.tar.gz
 cd /tmp
 tar -xvf telldus-core.tar.gz
