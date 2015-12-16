8ec559cdf4ab319f2de0a1ff7c6bcbebb412f504
Jon Börjesson <jon@spuun.se>
2015-12-16 12:15:41 +0000

    I have no idea what Im doing

--- travis-setup.sh
+++ travis-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-#sudo apt-get update -qq
-#sudo apt-get install -y libftdi1 libftdi-dev libconfuse0 libconfuse-dev
+export OLD_CXX=$CXX
+export CXX=gcc
 wget http://download.telldus.se/TellStick/Software/telldus-core/telldus-core-2.1.2.tar.gz -O /tmp/telldus-core.tar.gz
 cd /tmp
 tar -xvf telldus-core.tar.gz
@@ -8,3 +8,4 @@ cd telldus-core-2.1.2
 cmake .
 make 
 sudo make install 
+export CXX=$OLD_CXX
