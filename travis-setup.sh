40f787e76ba13fac4eec845872b6e20fdedf66df
Jon Börjesson <jon@spuun.se>
2015-12-16 12:29:40 +0000

    Use g++-4.8 to build telldus

--- travis-setup.sh
+++ travis-setup.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-export OLD_CXX=$CXX
-export CXX=gcc
 wget http://download.telldus.se/TellStick/Software/telldus-core/telldus-core-2.1.2.tar.gz -O /tmp/telldus-core.tar.gz
 cd /tmp
 tar -xvf telldus-core.tar.gz
@@ -8,4 +6,3 @@ cd telldus-core-2.1.2
 cmake .
 make 
 sudo make install 
-export CXX=$OLD_CXX
